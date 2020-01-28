4b8299a19d0b9a71146305038a17167818025cb1
Ale <amg_29-90@hotmail.com>
2020-01-27 23:49:46 -0600

    mejora-menu

--- 2020-2/sgb/js/main.js
+++ 2020-2/sgb/js/main.js
@@ -1,10 +1,14 @@
 // --------------efecto menu------------
 $(document).ready(function() {
+    $('ul li').click(function() {
+        $(this).siblings().removeClass('active');
+        $(this).toggleClass('active');
+    });
     $('.barras').click(function() {
         $('nav').toggleClass('active');
     });
 
-    var Header = $('#Header').offset().top,
+    var Header = $('#Header').offset().top - 67,
         Noticias = $('#Noticias').offset().top,
         Descripcion = $('#Descripcion').offset().top,
         Historial = $('#Historial').offset().top,
@@ -14,23 +18,21 @@ $(document).ready(function() {
         Bibliografia = $('#Bibliografia').offset().top,
         Contacto = $('#Contacto').offset().top;
     // Cumpleaños = $('#Cumple')
-
-
+    $(window).scroll(function() {
+        console.log($(this).scrollTop());
+        console.log($('#Header').offset().top);
+        if ($(this).scrollTop() > Header) {
+            $('#btn-header').css('background', '1269F7');
+        } else {
+            $('#btn-header').css('background', 'none');
+        }
+    });
 
     $('#btn-header').on('click', function(e) {
         e.preventDefault();
         $('html, body').animate({
             scrollTop: Header
         }, 1000);
-        // $('#btn-header2').css("background"), "#1269F7";
-        // $('#btn-noticias2').css("background"), "none";
-        // $('#btn-historial2').css("background"), "none";
-        // $('#btn-descripcion2').css("background"), "none";
-        // $('#btn-temario2').css("background"), "none";
-        // $('#btn-metodologia2').css("background"), "none";
-        // $('#btn-evaluacion2').css("background"), "none";
-        // $('#btn-bibliografia2').css("background"), "none";
-        // $('#btn-contacto2').css("background"), "none";
     });
     $('#btn-noticias').on('click', function(e) {
         e.preventDefault();
@@ -38,75 +40,65 @@ $(document).ready(function() {
             scrollTop: Noticias
         }, 1000);
 
-        // $('#btn-header2').css("background"), "none";
-        // $('#btn-noticias2').css("background"), "#1269F7";
-        // $('#btn-historial2').css("background"), "none";
-        // $('#btn-descripcion2').css("background"), "none";
-        // $('#btn-temario2').css("background"), "none";
-        // $('#btn-metodologia2').css("background"), "none";
-        // $('#btn-evaluacion2').css("background"), "none";
-        // $('#btn-bibliografia2').css("background"), "none";
-        // $('#btn-contacto2').css("background"), "none";
 
     });
     $('#btn-descripcion').on('click', function(e) {
         e.preventDefault();
         $('html, body').animate({ scrollTop: Descripcion }, 1000);
-        // $('#btn-header').css("background"), "#1269F7";
     });
     $('#btn-historial').on('click', function(e) {
         e.preventDefault();
         $('html, body').animate({
             scrollTop: Historial
         }, 1000);
-        // $('#btn-header').css("background"), "#1269F7";
+
     });
     $('#btn-temario').on('click', function(e) {
         e.preventDefault();
         $('html, body').animate({
             scrollTop: Temario
         }, 1000);
-        // $('#btn-header').css("background"), "#1269F7";
+
     });
     $('#btn-metodologia').on('click', function(e) {
         e.preventDefault();
         $('html, body').animate({
             scrollTop: Metodologia
         }, 1000);
-        // $('#btn-header').css("background"), "#1269F7";
+
     });
     $('#btn-evaluacion').on('click', function(e) {
         e.preventDefault();
         $('html, body').animate({ scrollTop: Evaluacion }, 1000);
-        // $('#btn-header').css("background"), "#1269F7";
+
     });
     $('#btn-bibliografia').on('click', function(e) {
         e.preventDefault();
         $('html, body').animate({
             scrollTop: Bibliografia
         }, 1000);
-        // $('#btn-header').css("background"), "#1269F7";
+
     });
     $('#btn-contacto').on('click', function(e) {
         e.preventDefault();
         $('html, body').animate({
             scrollTop: Contacto
         }, 1000);
-        // $('#btn-header').css("background"), "#1269F7";
+
     });
     $('#btn-Noticias').on('click', function(e) {
         e.preventDefault();
         $('html, body').animate({
             scrollTop: Noticias
         }, 1000);
-        // $('#btn-header').css("background"), "#1269F7";
+
     });
 
 
 });
 
-var reproductor = videojs('s-video', {
-    fluid: true,
-    autoplay: 'muted',
-    loop: true
-});
+// var reproductor = videojs('s-video', {
+//     fluid: true,
+//     autoplay: 'muted',
+//     loop: true
+// });
